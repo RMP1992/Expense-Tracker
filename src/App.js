baf3a9c4f482da0fc6c38da2deffc2fac3dbbf47
Ruben Peters1 <ruben.peters1@jdplc.com>
2021-07-16 00:40:13 +0100

    expense filter added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,38 @@ import Expenses from "./components/Expenses/Expenses";
 
 function App(props) {
   const expenses = [
-    {id:"e1", title: "Rent", amount: 530, date: new Date(2021, 4, 25)},
-    {id:"e2", title: "Council Tax", amount: 85, date: new Date(2021, 4, 25)},
-    {id:"e3", title: "PAYE Tax", amount: 132, date: new Date(2021, 4, 25)},
-    {id:"e4", title: "Future Finance", amount: 132, date: new Date(2021, 5, 9)},
+    {
+      id:"e1", 
+      title: "Rent", 
+      amount: 530, 
+      date: new Date(2021, 4, 25)
+    },
+    {
+      id:"e2", 
+      title: "Council Tax", 
+      amount: 85, 
+      date: new Date(2021, 4, 25)
+    },
+    {
+      id:"e3", 
+      title: "PAYE Tax", 
+      amount: 132, 
+      date: new Date(2021, 4, 25)
+    },
+    {
+      id:"e4", 
+      title: "Future Finance", 
+      amount: 132, 
+      date: new Date(2021, 5, 9)
+    },
   ]
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js')
+    console.log(expense)
+  }
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
