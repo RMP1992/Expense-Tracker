a0b88086f9a9983d0b3588847b64541879fc5a23
Ruben Peters1 <ruben.peters1@jdplc.com>
2021-07-22 00:06:34 +0100

    Finished

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2021');
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         
             <Card className="expenses">
                 <ExpenseFilter selected={filteredYear} onSaveSelectedYear={saveSelectedYear} />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
