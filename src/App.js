5e6cbd819c2ad5b15ed160a2e4a12171353de1b1
Ruben Peters1 <ruben.peters1@jdplc.com>
2021-07-21 12:03:57 +0100

    expense year filter functionality added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from "./components/Expenses/Expenses";
 
-function App(props) {
-  const expenses = [
-    {
-      id:"e1", 
-      title: "Rent", 
-      amount: 530, 
-      date: new Date(2021, 4, 25)
-    },
-    {
-      id:"e2", 
-      title: "Council Tax", 
-      amount: 85, 
-      date: new Date(2021, 4, 25)
-    },
-    {
-      id:"e3", 
-      title: "PAYE Tax", 
-      amount: 132, 
-      date: new Date(2021, 4, 25)
-    },
-    {
-      id:"e4", 
-      title: "Future Finance", 
-      amount: 132, 
-      date: new Date(2021, 5, 9)
-    },
-  ]
+const DUMMY_EXPENSES = [
+  {
+    id:"e1", 
+    title: "Rent", 
+    amount: 530, 
+    date: new Date(2021, 4, 25)
+  },
+  {
+    id:"e2", 
+    title: "Council Tax", 
+    amount: 85, 
+    date: new Date(2021, 4, 25)
+  },
+  {
+    id:"e3", 
+    title: "PAYE Tax", 
+    amount: 132, 
+    date: new Date(2021, 4, 25)
+  },
+  {
+    id:"e4", 
+    title: "Future Finance", 
+    amount: 132, 
+    date: new Date(2021, 5, 9)
+  },
+]
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  
   const addExpenseHandler = (expense) => {
-    console.log('In App.js')
-    console.log(expense)
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    })
   }
   return (
     <div>
--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseFilter.css';
 
 const ExpenseFilter = (props) => {
+    
     const dropdownChangeHandler = (event) => {
         
         props.onSaveSelectedYear(event.target.value);
